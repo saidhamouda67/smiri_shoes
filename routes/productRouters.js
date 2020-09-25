f5b53d317bdf5fc6ff8d9525eae7ccdc5f960921
medsaidraoudh <saidhamouda67@yahoo.fr>
2020-09-25 16:58:24 +0100

    other

--- routes/productRouters.js
+++ routes/productRouters.js
@@ -11,14 +11,14 @@ router.post('/Add-product-details/:id_prod',authController.protect,authControlle
 router.get('/',productController.getAllProducts)
 router.get('/product-details',authController.protect,authController.restrictTo('admin','manager'),productController.getAllProductDetails)
 router.get('/product-details/:id',productController.getProductDetail)
-router.patch('/product-details/:id',authController.protect,authController.restrictTo('admin'),productController.updateProductDetails)
+router.patch('/product-details/:id',authController.protect,authController.restrictTo('admin','manager'),productController.updateProductDetails)
 router.delete('/product-details/:id',productController.DeleteProductDetails)
 router.get('/:id',productController.getProduct)
-router.delete('/:id',authController.protect,authController.restrictTo('admin'),productController.DeleteProduct)
+router.delete('/:id',authController.protect,authController.restrictTo('admin','manager'),productController.DeleteProduct)
 router.patch('/:id',authController.protect,authController.restrictTo('admin','manager'),upload.array('images',4),productController.updateProduct);
 router.delete('/related-details/:id_prod',productController.DeleteRelatedDetails)
-router.get('/related-details/:id_prod',authController.protect,authController.restrictTo('admin'),productController.getRelatedDetails)
-router.patch('/link/:id_details/:id_prod',authController.protect,authController.restrictTo('admin'),productController.LinkDetails)
+router.get('/related-details/:id_prod',authController.protect,authController.restrictTo('admin','manager'),productController.getRelatedDetails)
+router.patch('/link/:id_details/:id_prod',authController.protect,authController.restrictTo('admin','manager'),productController.LinkDetails)
 
 
 
